88227737cf396ac074cc1138c246c86e31833315
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-14 19:01:06 +0300

    Create the prototype

--- js/object-create.js
+++ js/object-create.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// Use object.create to implement prototypal inheritance
+const now = new Date().getFullYear();
+
+const PersonProto = {
+  calcAge() {
+    return now - this.birthYear;
+  },
+};
