499ad21ba4e102161f7511d69fde357a49c67c8c
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-14 17:04:15 +0300

    Adjust comment

--- js/getters-setters.js
+++ js/getters-setters.js
@@ -58,7 +58,7 @@ console.log(dan.fullName);
 const denis = new Memeber('Denis Obua', 1993);
 console.log(denis.fullName);
 
-// Create stati method on Member constructor 
+// Create static method on Member constructor 
 Memeber.hey = function () {
   console.log('Hey there! 🖐️');
   console.log(this);
