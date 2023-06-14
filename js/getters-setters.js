bb42ceb8dcf079dc430bd802a57ef31d40a6c213
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-14 16:09:51 +0300

    Implement getters in a class

--- js/getters-setters.js
+++ js/getters-setters.js
@@ -17,3 +17,20 @@ console.log(acct.latestTxn);
 acct.latestTxn = 500;
 console.log(acct.latestTxn);
 console.log(acct.txns);
+
+// Use getters and setters on a class
+const now = new Date().getFullYear();
+
+class Memeber {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  get age() {
+    return now - this.birthYear;
+  }
+}
+
+const jasinta = new Memeber('Jasinta', 1963);
+console.log(jasinta.age);
