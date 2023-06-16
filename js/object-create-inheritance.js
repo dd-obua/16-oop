919ed1bbda739adba71132beaa2b232f95fd9c1e
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 15:56:27 +0300

    Create PersonProto class

--- js/object-create-inheritance.js
+++ js/object-create-inheritance.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const now = new Date().getFullYear();
+
+class PersonProto {
+  calcAge() {
+    return now - this.birthYear();
+  }
+
+  init(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+}
