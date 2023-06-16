4b770d1f98104910c32ddf5ba68d43e2730cb068
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 19:41:44 +0300

    Create Account class

--- js/encapsulation-protected.js
+++ js/encapsulation-protected.js
@@ -1,1 +1,33 @@
 'use strict';
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.txns = [];
+    this.locale = navigator.language;
+
+    console.log(`Thank you for opening an account ${this.owner}`);
+  }
+
+  transact(val) {
+    this.txns.push(val);
+  }
+
+  deposit(val) {
+    this.transact(val);
+  }
+
+  withdraw(val) {
+    this.transact(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) this.deposit(val);
+  }
+}
