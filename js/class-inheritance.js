b299195254d9d4b80580135af3e5a09edfd78b4d
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 13:26:16 +0300

    Create Person class

--- js/class-inheritance.js
+++ js/class-inheritance.js
@@ -2,3 +2,34 @@
 
 const now = new Date().getFullYear();
 
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return now - this.birthYear;
+  }
+
+  greet() {
+    return `Hey ${this.firstName}.`;
+  }
+
+  getAge() {
+    return now - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else console.log(`${this.fullName} is not a full name.`);
+  }
+
+  set fullName(name) {
+    return this._fullName;
+  }
+
+  static hey() {
+    return `Hey there :raised_hand_with_fingers_splayed:`;
+  }
+}
