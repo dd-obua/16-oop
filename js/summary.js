d0680a64672bcdf2315d822701093eb1b1a589cf
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-17 12:21:17 +0300

    Create Student class

--- js/summary.js
+++ js/summary.js
@@ -12,3 +12,41 @@ class Person {
     return now - this.birthYear;
   }
 }
+
+class Student extends Person {
+  university = 'Gulu University';
+  #studyHours = 0;
+  #course;
+  static numSubjects = 10;
+
+  constructor(fullName, birthYear, startYear, course) {
+    super(fullName, birthYear);
+    this.startYear = startYear;
+    this.#course = course;
+  }
+
+  introduce() {
+    return `I study ${this.#course} at ${this.university}`;
+  }
+
+  study(h) {
+    this.#makeCoffee();
+    this.#studyHours += h;
+  }
+
+  #makeCoffe() {
+    return `Here is a coffee for you.`;
+  }
+
+  get testScore() {
+    return _this.testScore;
+  }
+
+  set testScore(score) {
+    this._testScore = score <= 20 ? score : 0;
+  }
+
+  static printCurriculum() {
+    console.log(`There are ${this.numSubjects} subjects.`);
+  }
+}
