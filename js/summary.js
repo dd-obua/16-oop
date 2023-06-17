06e8a6bebdb2f6b92cbca7679ef7450616343911
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-17 12:12:26 +0300

    Create Person class

--- js/summary.js
+++ js/summary.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const now = new Date().getFullYear();
+
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return now - this.birthYear;
+  }
+}
